5d1bade64367c17860b758d48592bc793d527b3a
Emilia <e.lubanska@gmail.com>
2021-05-16 15:52:08 -0400

    Update loopy-lighthouse.js

--- loopy-lighthouse.js
+++ loopy-lighthouse.js
@@ -1,7 +1,11 @@
-for (let i = 100; i<=200; i++){
-    if (i % 3){
+for (let i = 100; i<=150; i++){
+    if ((!(i % 3)) && (!(i % 4))){
+        console.log("LoopyLighthouse")
+    } else if (!(i % 4)){
+        console.log("Lighthouse")
+    } else if (!(i % 3)){
         console.log("Loopy")
-
+    } else {
+        console.log(i)
     }
-    //console.log(i)
-}
+   }
