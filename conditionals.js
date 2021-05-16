d590bc19b6749c190419c5d22d657abb9c54f620
Emilia <e.lubanska@gmail.com>
2021-05-16 01:46:59 +0000

    More examples

--- conditionals.js
+++ conditionals.js
@@ -10,3 +10,10 @@ if (cold) {
 }
 
 console.log("Now you're ready to go outside!");
+
+const temperature = 45;
+
+if (temperature < -40 || temperature > 40){
+	console.log("Maybe going outside isn't such a great idea")
+}
+
