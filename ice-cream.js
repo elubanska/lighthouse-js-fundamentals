78f8d7d9fac211e4b1bd981baad38065a7991094
Emilia <e.lubanska@gmail.com>
2021-05-17 13:57:51 -0400

    arrays

---
+++ ice-cream.js
@@ -0,0 +1,13 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+console.log(iceCreamFlavours[0]);
+const len = iceCreamFlavours.length;
+console.log(iceCreamFlavours[len-1]);
+console.log(len);
+console.log("Bob" || " Marley");
+console.log("Jeremy" > "Khurram");
+console.log("Don\nBurks");
+"15" === 15;
