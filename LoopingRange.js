96851bf1b700bf4840e052d4c2d00623fc56d1d9
Emilia <e.lubanska@gmail.com>
2021-05-17 14:26:48 -0400

    Create LoopingRange.js

---
+++ LoopingRange.js
@@ -0,0 +1,10 @@
+function range(start, end, step){
+    let list = [];
+    for (let i = start; i <= end; i = i + step){
+        list.push(i);
+    }
+    return list;
+}
+console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
